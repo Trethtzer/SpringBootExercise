523dd62e204be978a565c76b9f221dcea5608a8f
Trethtzer <gsilesbonilla@gmail.com>
2020-10-03 11:49:51 +0200

    Add Account model

---
+++ src/main/java/com/silesbonilla/springbootexercise/model/Account.java
@@ -0,0 +1,76 @@
+package com.silesbonilla.springbootexercise.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	@Column(name = "currency", nullable = false)
+	private int currency;
+	
+	@Column(name = "money", nullable = false)
+	private float money;
+	
+	@Column(name = "treasury", nullable = false)
+	private boolean treasury;
+	
+	public Account() {
+		
+	}
+	
+	public Account(String name, int currency, float money, boolean treasury) {
+		this.name = name;
+		this.currency = currency;
+		this.money = money;
+		this.treasury = treasury;
+	}
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCurrency() {
+		return currency;
+	}
+	public void setCurrency(int currency) {
+		this.currency = currency;
+	}
+
+	public float getMoney() {
+		return money;
+	}
+	public void setMoney(float money) {
+		this.money = money;
+	}
+
+	public boolean isTreasury() {
+		return treasury;
+	}
+	public void setTreasury(boolean treasury) {
+		this.treasury = treasury;
+	}
+	
+}
