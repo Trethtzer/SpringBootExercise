25dfae58e4cc8d1f828c46dc2f78396802a15532
Trethtzer <gsilesbonilla@gmail.com>
2020-10-03 12:15:10 +0200

    Create account service and fix minor typo

--- src/main/java/com/silesbonilla/springbootexercise/controller/AccountController.java
+++ src/main/java/com/silesbonilla/springbootexercise/controller/AccountController.java
@@ -5,8 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +28,7 @@ public class AccountController {
 		return accountRepository.findAll();
 	}
 	
-	@GetMapping("account/{id}")
+	@GetMapping("accounts/{id}")
 	public ResponseEntity<Account> getAccountById(@PathVariable(value = "id") Long accountId){
 		
 		Optional<Account> account = accountRepository.findById(accountId);
@@ -37,4 +40,9 @@ public class AccountController {
 		}        
     }
 	
+	@PostMapping("/accounts")
+    public ResponseEntity<Account> createAccount(@Validated @RequestBody Account account) {        
+		accountRepository.save(account);
+		return ResponseEntity.ok().body(account);
+    }
 }
